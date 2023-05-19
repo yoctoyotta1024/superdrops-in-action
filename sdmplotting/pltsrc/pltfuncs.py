f5ca2e0268ff6074cf2bf63e0e93ab46f33c4456
Clara Levante <yoctoyotta1024@yahoo.com>
2023-05-19 02:39:07 +0200

    tidied up remove spines function

--- sdmplotting/pltsrc/pltfuncs.py
+++ sdmplotting/pltsrc/pltfuncs.py
@@ -24,26 +24,26 @@ def ticks_withinlims(ticks, ticklabels, lims):
 
 def remove_spines_set_fontsize(axs, labels, ticks, legend):
 
+
   for a, ax in enumerate(axs):
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     
-    ax.set_xlabel(ax.get_xlabel(), fontsize=labels)
-    ax.set_ylabel(ax.get_ylabel(), fontsize=labels) 
 
     xlims = ax.get_xlim() 
     ylims = ax.get_ylim() 
-    ax.set_xlim(xlims)
-    ax.set_ylim(ylims)
 
     xticks, xticklabels = ticks_withinlims(ax.get_xticks(),
                                            ax.get_xticklabels(), xlims) 
     yticks, yticklabels = ticks_withinlims(ax.get_yticks(),
                                            ax.get_yticklabels(), ylims) 
     
+    ax.set_xlim(xlims), ax.set_ylim(ylims)
     ax.set_xticks(xticks, xticklabels, fontsize=ticks)
     ax.set_yticks(yticks, yticklabels, fontsize=ticks)
 
+    ax.set_xlabel(ax.get_xlabel(), fontsize=labels)
+    ax.set_ylabel(ax.get_ylabel(), fontsize=labels) 
 
     if ax.get_legend():
       ax.legend(fontsize=legend)
