dd7c4493c8b5802ea7577034bce297a11826ca2c
Clara Levante <yoctoyotta1024@yahoo.com>
2023-05-21 15:05:28 +0200

    some useful paths commented

--- quickplots/paths4ipynbs.py
+++ quickplots/paths4ipynbs.py
@@ -1,17 +1,19 @@
 ### paths and file names often required by ipynb plotting scripts ###
 
+
 HOMEdir = "/home/m/m300950"
 path2sds = HOMEdir+"/superdrops_in_action"
-# path2build = HOMEdir+"/CLEO/build"
-path2build = path2sds+"/quickplots/condensationmethod/shimacrit1/"
-path2dataset = path2build+"matsu_10iters/bin"
+path2build = HOMEdir+"/CLEO/build"
+# path2build = path2sds+"/quickplots/condensationmethod/"+stem
+path2dataset = path2build+"/bin/"
 
 gridfile = path2build+"/share/dimlessGBxboundaries.dat"
 setuptxt = path2dataset+"/setup.txt"
 dataset = path2dataset+"/SDMdata.zarr"
 
 stem = "test"
-# savedir = path2sds+"/quickplots/"+stem+"plots/"
-savedir = path2sds+"/quickplots/condensationmethod/shimacrit1/matsu_10iters/plots/"
+savedir = path2sds+"/quickplots/"+stem+"plots/"
+# stem = "substeptests/supersat_0p1ssubstep_v2/"
+# savedir = path2sds+"/quickplots/condensationmethod/"+stem+"/plots/"
 
 print("build: "+path2build+"\ndataset: "+dataset+"\nsavedir: "+savedir)
