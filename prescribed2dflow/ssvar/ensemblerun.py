db7901a70fe7598c12969937b1923b9da016e2a3
Clara Levante <yoctoyotta1024@yahoo.com>
2023-05-22 08:35:27 +0200

    py script for runnign ensemble of supersat experiments

---
+++ prescribed2dflow/ssvar/ensemblerun.py
@@ -0,0 +1,195 @@
+# for 128 SD randomly distirbuted in < 750m of domain, vary initial
+# supersaturation profile (sbelow and sabove) to see effect on rain
+# distribution and initiation time. e.g. higher supersaturation above
+# zbase (sabove) means faster condnsational growth and therefore rain
+# formation sonner in updraught whereas lower supersaturation -> slower
+# condenstinal growth -> later rain onset and in downdraught
+
+import os
+import sys
+import numpy as np
+from pathlib import Path 
+
+path2CLEO = "/home/m/m300950/CLEO/"
+sys.path.append(path2CLEO)
+
+from pySD.gbxboundariesbinary_src import create_gbxboundaries as  cgrid
+from pySD.gbxboundariesbinary_src import read_gbxboundaries as rgrid
+from pySD.thermobinary_src import thermogen
+from pySD.thermobinary_src import create_thermodynamics as cthermo
+from pySD.thermobinary_src import read_thermodynamics as rthermo
+from pySD.initsuperdropsbinary_src import initattributes as iSDs
+from pySD.initsuperdropsbinary_src import radiiprobdistribs as rprobs 
+from pySD.initsuperdropsbinary_src import create_initsuperdrops as csupers 
+from pySD.initsuperdropsbinary_src import read_initsuperdrops as rsupers 
+from pySD import editconfigfile 
+
+isgenSDsGBxs = True # create gridbox bounadires and SD binaries
+isgenThermo  = True  # create thermodynamics files
+isfigures = [True, True]
+
+runids = range(0,1,1) # numbers of for initial SD conditions
+sratios_experiments = { # s_ratio [below, above] Zbase for each experiment
+   "ss0p65_1p001" : [0.65, 1.001]
+}
+
+
+### ---------------------------------------------------------------- ###
+### paths and filenames for inputs and outputs
+currentdir = "/home/m/m300950/superdrops_in_action/prescribed2dflow/ssvar/"
+path2build = "/work/mh1126/m300950/prescribed2dflow/ssvar/build/"
+binariespath = path2build+"/share/"
+
+constsfile = path2CLEO+"libs/claras_SDconstants.hpp"
+configfile = currentdir+"/ssvarconfig.txt"
+gridfile =  binariespath+"/dimlessGBxbounds.dat" # note this should match config.txt
+
+savefigpath = currentdir
+binpath = path2build+"../bin/"
+
+### input parameters for gridbox boundaries
+zgrid                = [0, 1500, 50]
+xgrid                = [0, 1500, 50]
+ygrid                = np.asarray([0, 20])
+
+### input parameters for superdroplets
+zlim                 = 100
+npergbx              = 256
+
+coord3gen            = iSDs.SampleCoordGen(True) 
+coord1gen            = iSDs.SampleCoordGen(True) 
+coord2gen            = None                        
+
+rspan                = [3e-9, 3e-6]                
+randomr              = True                        
+radiigen             = iSDs.SampleDryradiiGen(rspan, randomr) 
+geomeans             = [0.02e-6, 0.15e-6]               
+geosigs              = [1.4, 1.6]                    
+scalefacs            = [6e6, 4e6]   
+numconc              = np.sum(scalefacs)
+radiiprobdist        = rprobs.LnNormal(geomeans, geosigs, scalefacs)
+
+### input parameters for thermodynamic profiles
+PRESS0               = 101500    # [Pa]
+THETA                = 289       # [K]
+qcond                = 0.0       # [Kg/Kg]
+WMAX                 = 0.6       # [m/s]
+VVEL                 = None      # [m/s]
+Zlength              = 1500      # [m]
+Xlength              = 1500      # [m]
+qvapmethod           = "sratio"
+Zbase                = 750       # [m]
+moistlayer = {
+                 "z1": 750, # [m]
+                 "z2": 850, # [m]
+                 "x1": 0,   # [m]
+                 "x2": 750, # [m]
+           "mlsratio": 1.01
+}
+### ---------------------------------------------------------------- ###
+
+if path2CLEO == path2build:
+  raise ValueError("build directory cannot be CLEO")
+else:
+  Path(path2build).mkdir(exist_ok=True) 
+  Path(binariespath).mkdir(exist_ok=True) 
+  Path(binpath).mkdir(exist_ok=True) 
+
+# write initial conditions / setup binaries
+if isgenSDsGBxs:
+  cgrid.write_gridboxboundaries_binary(gridfile, zgrid, xgrid, ygrid, constsfile)
+
+  nsupers = iSDs.nsupers_at_domain_base(gridfile, constsfile, npergbx, zlim)
+  initattrsgen = iSDs.InitManyAttrsGen(radiigen, radiiprobdist,
+                                        coord3gen, coord1gen, coord2gen)
+  for n in runids: 
+    initSDsfile = binariespath+"/run"+str(n)+"_dimlessSDsinit.dat" # note this should match config.txt
+    csupers.write_initsuperdrops_binary(initSDsfile, initattrsgen, 
+                                        configfile, constsfile,
+                                        gridfile, nsupers, numconc)
+
+  ### plot initial conditions / setup
+  if isfigures[0]:
+      if isfigures[1]:
+          Path(savefigpath).mkdir(exist_ok=True) 
+      
+      rgrid.print_domain_info(constsfile, gridfile)
+      rgrid.plot_gridboxboundaries(constsfile, gridfile, savefigpath, isfigures[1])
+
+      rsupers.plot_initGBxsdistribs(configfile, constsfile,
+                                                     initSDsfile, gridfile,
+                                                     savefigpath, isfigures[1], 0)
+
+### generate thermodyanmics for each s_ratios experiment
+if isgenThermo:
+  for exp, sratios in sratios_experiments.items():
+    thermopath = binariespath+"/"+exp+"/"
+    
+    Path(thermopath).mkdir(exist_ok=True) 
+    thermofiles =  thermopath+"dimless.dat"
+    thermodyngen = thermogen.ConstHydrostaticAdiabat(configfile, constsfile, PRESS0, 
+                                          THETA, qvapmethod, sratios, Zbase,
+                                          qcond, WMAX, Zlength, Xlength,
+                                          VVEL, moistlayer)
+    
+    print("experiment: ", exp, "sratios:", sratios, "in thermofiles:", thermofiles)
+    cthermo.write_thermodynamics_binary(thermofiles, thermodyngen, configfile,
+                                        constsfile, gridfile)
+
+### run model for each s_ratios experiment
+print("--- compiling runCLEO ---\nin "+path2build)
+os.chdir(path2build)
+os.system("make -j 16")
+os.chdir(currentdir)
+
+for exp, sratios in sratios_experiments.items():
+  
+  ### names of initial condiitons / setup binarues
+  grid_filename = gridfile
+  thermo_filenames = binariespath+"/"+exp+"/dimless.dat"
+
+  ### where to output data
+  binpath_exp = binpath+"/"+exp+"/"  
+  Path(binpath_exp).mkdir(exist_ok=True) 
+    
+  if isfigures[0]:
+      savefigpath_exp = savefigpath+"/"+exp+"/"
+      if isfigures[1]:
+          Path(savefigpath).mkdir(exist_ok=True) 
+      rthermo.plot_thermodynamics(constsfile, configfile, gridfile,
+                                  thermo_filenames, savefigpath_exp,
+                                  isfigures[1])
+  
+  for n in runids:  
+    initSDs_filename = binariespath+"/run"+str(n)+"_dimlessSDsinit.dat"
+    setuptxt = binpath_exp+"run"+str(n)+"setup.txt"                 
+    zarrbasedir = binpath_exp+"run"+str(n)+"SDMdata.zarr"  
+
+    print(" --- exp "+exp+" run "+str(n)+" ---")
+    print("gridfile:", grid_filename)
+    print("thermofiles:", thermo_filenames[:-4]+"_[XXX].dat")
+    print("initSDs:", initSDs_filename)
+    print("output to: "+setuptxt+"\n           "+zarrbasedir)
+
+    ### modify config file ###
+    configparams2edit = {
+      "initSDs_filename" : initSDs_filename,
+      "grid_filename" : grid_filename,
+      "setuptxt" : setuptxt,
+      "zarrbasedir" : zarrbasedir,
+      "press_filename" : thermo_filenames[:-4]+"_press.dat", 
+      "temp_filename" : thermo_filenames[:-4]+"_temp.dat",
+      "qvap_filename" : thermo_filenames[:-4]+"_qvap.dat",
+      "qcond_filename" : thermo_filenames[:-4]+"_qcond.dat",
+      "wvel_filename" : thermo_filenames[:-4]+"_wvel.dat",
+      "uvel_filename" : thermo_filenames[:-4]+"_uvel.dat",
+      "vvel_filename" : thermo_filenames[:-4]+"_vvel.dat",
+      }
+    
+    editconfigfile.edit_config_params(configfile, configparams2edit)
+    print("- executing runCLEO via sbatch -")
+    execute = path2build+"src/runCLEO "+configfile+" "+constsfile
+    os.system("echo "+execute)
+    os.system(execute)
+    print("-----------------------")
+### ---------------------------------------------------------------- ###
