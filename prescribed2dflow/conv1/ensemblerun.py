6348cf52214e72d78c0422f555e136d23571c28b
Clara Levante <yoctoyotta1024@yahoo.com>
2023-05-26 08:57:01 +0200

    runnign 4 sds per gbx experiment

--- prescribed2dflow/conv1/ensemblerun.py
+++ prescribed2dflow/conv1/ensemblerun.py
@@ -17,7 +17,7 @@ isfigures = [True, True]
 
 runids = range(0,10,1) # numbers of for initial SD conditions
 experimentids = { # number of SDs per GBx initially (in gbxs with SDs)
-   "n1" : 1,
+   "n4" : 4,
 }
 sumbit_individruns = False # submit each run of an experiment as seperate SLURM jobs
 
