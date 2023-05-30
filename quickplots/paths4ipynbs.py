7cb72aca4648f0ad8ea65d73f209cd70f9d18edf
Clara Levante <yoctoyotta1024@yahoo.com>
2023-05-30 09:29:18 +0200

    comment in dataset to use

--- quickplots/paths4ipynbs.py
+++ quickplots/paths4ipynbs.py
@@ -1,33 +1,28 @@
 ### paths and file names often required by ipynb plotting scripts ###
 
-# HOMEdir = "/home/m/m300950"
-# path2sds = HOMEdir+"/superdrops_in_action"
-# path2build = HOMEdir+"/CLEO/build/"
-# path2dataset = path2build+"/bin/"
-# gridfile = path2build+"/share/dimlessGBxboundaries.dat"
-# setuptxt = path2dataset+"/setup.txt"
-# dataset = path2dataset+"/SDMdata.zarr"
-# stem = ""
-# savedir = path2sds+"/quickplots/"+stem+"plots/"
-
-
 HOMEdir = "/home/m/m300950"
 path2sds = HOMEdir+"/superdrops_in_action/"
-path2build = "/work/mh1126/m300950/prescribed2dflow/conv1/build/"
+# path2build = "/work/mh1126/m300950/prescribed2dflow/conv1/build/"
+path2build = HOMEdir+"/CLEO/build/"
 
 exp = "n8"
 # savedir = path2sds+"prescribed2dflow/conv1/"+exp+"/run0/"
 savedir = path2sds+"prescribed2dflow/conv1/compareplots/"
+# savedir = path2sds+"/quickplots/plots/"
 print("builddir: "+path2build+"\nsavedir: "+savedir)
 
 class DatasetPaths:
   def __init__(self):
         
-    path2dataset = path2build+"../bin/"+exp+"/"
+    # path2dataset = path2build+"../bin/"+exp+"/"
+    # self.gridfile = path2build+"/share/dimlessGBxbounds.dat"
+    # self.setuptxt = path2dataset+"/run0setup.txt"
+    # self.dataset = path2dataset+"/run0SDMdata.zarr"
     
-    self.gridfile = path2build+"/share/dimlessGBxbounds.dat"
-    self.setuptxt = path2dataset+"/run0setup.txt"
-    self.dataset = path2dataset+"/run0SDMdata.zarr"
+    path2dataset = path2build+"/bin/"
+    self.gridfile = path2build+"/share/dimlessGBxboundaries.dat"
+    self.setuptxt = path2dataset+"/setup.txt"
+    self.dataset = path2dataset+"/SDMdata.zarr"
     
     print("dataset: "+self.dataset,
           "\ngridfile: "+self.gridfile,
