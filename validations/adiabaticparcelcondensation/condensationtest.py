d0b1ce6af096b9ce56535f75a7d8747454103c2b
Clara Levante <yoctoyotta1024@yahoo.com>
2023-05-31 02:48:14 +0200

    reformatting condensation validations

--- validations/adiabaticparcelcondensation/condensationtest.py
+++ validations/adiabaticparcelcondensation/condensationtest.py
@@ -4,32 +4,31 @@
 # S. Arabas and S. Shima 2017 to show
 # example of cusp birfucation
 
+# To create build dir:
+# CXX=[compiler choice] cmake -S [path2CLEO] -B ./build
+# e.g. CXX=g++-13 CC=gcc-13 cmake -S ../../../CLEO/ -B ./build
+# or CXX=g++-13 CC=gcc-13 cmake -S ../../../CLEO/ -B ./build -DKokkos_ENABLE_OPENMP=ON -DKokkos_ARCH_NATIVE=ON
+
 import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-# To create build dir:
-# CXX=[compiler choice] cmake -S [path2CLEO] -B ./build 
-# e.g. CXX=g++-13 CC=gcc-13 cmake -S ../../../CLEO/ -B ./build
-# or CXX=g++-13 CC=gcc-13 cmake -S ../../../CLEO/ -B ./build -DKokkos_ENABLE_OPENMP=ON -DKokkos_ARCH_NATIVE=ON
-
 # path2CLEO = "/Users/yoctoyotta1024/Documents/b1_springsummer2023/CLEO/"
 # apath = "/Users/yoctoyotta1024/Documents/b1_springsummer2023/superdrops_in_action/"
 path2CLEO = "/home/m/m300950/CLEO/"
 apath = "/home/m/m300950/superdrops_in_action/"
-
-sys.path.append(path2CLEO) # for imports from pySD package
+sys.path.append(path2CLEO)  # for imports from pySD package
 sys.path.append(apath+"sdmplotting/")
 sys.path.append(apath+"validations/")
 
-from pySD.gbxboundariesbinary_src import create_gbxboundaries as cgrid
-from pySD.gbxboundariesbinary_src import read_gbxboundaries as rgrid
 from pySD.initsuperdropsbinary_src import *
+from pySD.gbxboundariesbinary_src import read_gbxboundaries as rgrid
+from pySD.gbxboundariesbinary_src import create_gbxboundaries as cgrid
 from datsrc import *
-from validsrc import individSDs
 from validsrc import condensationcurves as ccs
+from validsrc import individSDs
 
 ############### INPUTS ##################
 # path and filenames for creating SD initial conditions and for running model
@@ -47,56 +46,62 @@ isfigures = [True, True]
 nsupers = {0: 1}
 coord_params = ["false"]
 zgrid = np.asarray([0, 100])
-xgrid = np.asarray([0, 100]) 
+xgrid = np.asarray([0, 100])
 ygrid = np.asarray([0, 100])
 
 # settings for monodisperse droplet radii
-numconc              = 0.5e9                        # [m^-3] total no. concentration of droplets
-monor                = 0.025e-6                        
-radiigen  = initattributes.MonoAttrsGen(monor)       # all SDs have the same dryradius = monor [m]
-radiiprobdist = radiiprobdistribs.DiracDelta(monor)  # monodisperse droplet radii probability distribution
-samplevol = rgrid.calc_domainvol(zgrid, xgrid, ygrid) # volume SD sample occupies (entire domain) [m^3]
-coord3gen            = None                        # do not generate superdroplet coords
-coord1gen            = None                        
-coord2gen            = None   
+# [m^-3] total no. concentration of droplets
+numconc = 0.5e9
+monor = 0.025e-6
+# all SDs have the same dryradius = monor [m]
+radiigen = initattributes.MonoAttrsGen(monor)
+# monodisperse droplet radii probability distribution
+radiiprobdist = radiiprobdistribs.DiracDelta(monor)
+# volume SD sample occupies (entire domain) [m^3]
+samplevol = rgrid.calc_domainvol(zgrid, xgrid, ygrid)
+coord3gen = None                        # do not generate superdroplet coords
+coord1gen = None
+coord2gen = None
 
 # path and file names for plotting results
 setupfile = binpath+"condsetup.txt"
 dataset = binpath+"condsol.zarr"
 
+
 def displacement(time, w_avg, thalf):
     '''displacement z given velocity, w, is sinusoidal 
     profile: w = w_avg * pi/2 * np.sin(np.pi * t/thalf)
     where wmax = pi/2*w_avg and tauhalf = thalf/pi.'''
-    
+
     zmax = w_avg / 2 * thalf
-    z = zmax * ( 1 - np.cos(np.pi * time / thalf) )
+    z = zmax * (1 - np.cos(np.pi * time / thalf))
     return z
 
+
 # ### 1. create files with initial SDs conditions and gridbox boundaries
-Path(binpath).mkdir(parents=True, exist_ok=True) 
+Path(binpath).mkdir(parents=True, exist_ok=True)
 os.system("rm "+gridfile)
-os.system("rm "+initSDsfile)            
-cgrid.write_gridboxboundaries_binary(gridfile, zgrid, xgrid, 
-                                                     ygrid, constsfile)
+os.system("rm "+initSDsfile)
+cgrid.write_gridboxboundaries_binary(gridfile, zgrid, xgrid,
+                                     ygrid, constsfile)
 rgrid.print_domain_info(constsfile, gridfile)
 
 initattrsgen = initattributes.InitManyAttrsGen(radiigen, radiiprobdist,
                                                coord3gen, coord1gen, coord2gen)
-create_initsuperdrops.write_initsuperdrops_binary(initSDsfile, initattrsgen, 
+create_initsuperdrops.write_initsuperdrops_binary(initSDsfile, initattrsgen,
                                                   configfile, constsfile,
                                                   gridfile, nsupers, numconc)
 
 
 if isfigures[0]:
-    rgrid.plot_gridboxboundaries(constsfile, gridfile, 
-                                              binpath, isfigures[1])
+    rgrid.plot_gridboxboundaries(constsfile, gridfile,
+                                 binpath, isfigures[1])
     read_initsuperdrops.plot_initGBxsdistribs(configfile, constsfile, initSDsfile,
                                               gridfile, binpath, isfigures[1], "all")
 plt.close()
 
-### 2. compile and run model
-Path(buildpath).mkdir(exist_ok=True) 
+# 2. compile and run model
+Path(buildpath).mkdir(exist_ok=True)
 os.chdir(buildpath)
 os.system('pwd')
 os.system('rm -rf '+dataset)
@@ -107,39 +112,41 @@ os.system(buildpath+'src/cond0D ' + configfile+' '+constsfile)
 # read in constants and intial setup from setup .txt file
 setup, grid = pysetuptxt.get_setup_gridinfo(setupfile, gridfile)
 SDprops = sdprops.CommonSuperdropProperties(setup["RHO_L"], setup["RHO_SOL"],
-                                             setup["MR_SOL"], setup["IONIC"])
+                                            setup["MR_SOL"], setup["IONIC"])
 thermo = pyzarr.get_thermodata(dataset, setup, grid["ndims"])
 time = pyzarr.get_time(dataset).secs
 sddata = pyzarr.get_sddata(dataset)
 zprof = displacement(time, setup["W_AVG"], setup["T_HALF"])
 
 # relative humidty and supersaturation
-press = thermo.press*100 #convert from hPa to Pa
-relh, supersat = thermoeqns.relative_humidity(press, thermo.temp, 
+press = thermo.press*100  # convert from hPa to Pa
+relh, supersat = thermoeqns.relative_humidity(press, thermo.temp,
                                               thermo.qvap, setup["Mr_ratio"])
-                                              
-minid, maxid = 0, setup["totnsupers0"] # sample drops to plot from whole range of SD ids
+
+# sample drops to plot from whole range of SD ids
+minid, maxid = 0, setup["totnsupers0"]
 ndrops2plot = setup["totnsupers0"]
-radii = pyzarr.attrtimeseries_for_superdropssample(sddata, "radius", ndrops2plot, minid, maxid) 
+radii = pyzarr.attrtimeseries_for_superdropssample(
+    sddata, "radius", ndrops2plot, minid, maxid)
 fig, ax = individSDs.individ_radiusgrowths_figure(time, radii)
 savename = "cond_SDsradiigrowth.png"
-fig.savefig(binpath+savename, dpi=400, 
+fig.savefig(binpath+savename, dpi=400,
             bbox_inches="tight", facecolor='w', format="png")
 print("Figure .png saved as: "+binpath+savename)
-plt.show()    
+plt.show()
 
 radius = pyzarr.attrtimeseries_for_1superdrop(sddata, 0, "radius")
 eps = pyzarr.attrtimeseries_for_1superdrop(sddata, 0, "eps")
 m_sol = pyzarr.attrtimeseries_for_1superdrop(sddata, 0, "m_sol")
 
-numconc = np.sum(sddata["eps"][0])/grid["domainvol"]/1e6 # [/cm^3]
+numconc = np.sum(sddata["eps"][0])/grid["domainvol"]/1e6  # [/cm^3]
 
 fig, axs = ccs.condensation_validation_figure(time, eps, radius, m_sol,
-                                                thermo.temp.flatten(),
-                                                supersat.flatten(), zprof, SDprops,
-                                                setup, numconc)
+                                              thermo.temp.flatten(),
+                                              supersat.flatten(), zprof, SDprops,
+                                              setup, numconc)
 savename = "cond_validation.png"
-fig.savefig(binpath+savename, dpi=400, 
+fig.savefig(binpath+savename, dpi=400,
             bbox_inches="tight", facecolor='w', format="png")
 print("Figure .png saved as: "+binpath+savename)
-plt.show()            
+plt.show()
--- validations/arabas_shima_2017/arabastest.py
+++ validations/arabas_shima_2017/arabastest.py
@@ -4,30 +4,29 @@
 # S. Arabas and S. Shima 2017 to check radius
 # growth due to condensation is correct
 
+# To create build dir:
+# CXX=[compiler choice] cmake -S [path2CLEO] -B ./build
+# e.g. CXX=g++-13 CC=gcc-13 cmake -S ../../../CLEO/ -B ./build
+# or CXX=g++-13 CC=gcc-13 cmake -S ../../../CLEO/ -B ./build -DKokkos_ENABLE_OPENMP=ON -DKokkos_ARCH_NATIVE=ON
+
 import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-# To create build dir:
-# CXX=[compiler choice] cmake -S [path2CLEO] -B ./build 
-# e.g. CXX=g++-13 CC=gcc-13 cmake -S ../../../CLEO/ -B ./build
-# or CXX=g++-13 CC=gcc-13 cmake -S ../../../CLEO/ -B ./build -DKokkos_ENABLE_OPENMP=ON -DKokkos_ARCH_NATIVE=ON
-
 # path2CLEO = "/Users/yoctoyotta1024/Documents/b1_springsummer2023/CLEO/"
 # apath = "/Users/yoctoyotta1024/Documents/b1_springsummer2023/superdrops_in_action/"
 path2CLEO = "/home/m/m300950/CLEO/"
 apath = "/home/m/m300950/superdrops_in_action/"
-
-sys.path.append(path2CLEO) # for imports from pySD package
+sys.path.append(path2CLEO)  # for imports from pySD package
 sys.path.append(apath+"sdmplotting/")
 sys.path.append(apath+"validations/")
 
-from pySD.gbxboundariesbinary_src import create_gbxboundaries as cgrid
-from pySD.gbxboundariesbinary_src import read_gbxboundaries as rgrid
+from pySD import editconfigfile
 from pySD.initsuperdropsbinary_src import *
-from pySD import editconfigfile 
+from pySD.gbxboundariesbinary_src import read_gbxboundaries as rgrid
+from pySD.gbxboundariesbinary_src import create_gbxboundaries as cgrid
 from datsrc import *
 from validsrc import condensationcurves as ccs
 
@@ -48,16 +47,18 @@ isfigures = [True, False]
 nsupers = {0: 32}
 coord_params = ["false"]
 zgrid = np.asarray([0, 100])
-xgrid = np.asarray([0, 100]) 
+xgrid = np.asarray([0, 100])
 ygrid = np.asarray([0, 100])
 
 # settings for monodisperse droplet radii
-numconcs              = [500e6, 500e6, 50e6]                        # [m^-3] total no. concentration of droplets
-monors                = [0.05e-6, 0.1e-6, 0.1e-6]                
-coord3gen            = None                        # do not generate superdroplet coords
-coord1gen            = None                        
-coord2gen            = None   
-samplevol = rgrid.calc_domainvol(zgrid, xgrid, ygrid)  # volume SD sample occupies (entire domain) [m^3]
+# [m^-3] total no. concentration of droplets
+numconcs = [500e6, 500e6, 50e6]
+monors = [0.05e-6, 0.1e-6, 0.1e-6]
+coord3gen = None                        # do not generate superdroplet coords
+coord1gen = None
+coord2gen = None
+# volume SD sample occupies (entire domain) [m^3]
+samplevol = rgrid.calc_domainvol(zgrid, xgrid, ygrid)
 
 # setup parameters
 params1 = {
@@ -85,17 +86,19 @@ params3 = {
 
 paramslist = [params1, params2, params3]
 
+
 def displacement(time, w_avg, thalf):
     '''displacement z given velocity, w, is sinusoidal 
     profile: w = w_avg * pi/2 * np.sin(np.pi * t/thalf)
     where wmax = pi/2*w_avg and tauhalf = thalf/pi.'''
-    
+
     zmax = w_avg / 2 * thalf
-    z = zmax * ( 1 - np.cos(np.pi * time / thalf) )
+    z = zmax * (1 - np.cos(np.pi * time / thalf))
     return z
 
+
 # ### 1. compile model
-Path(buildpath).mkdir(exist_ok=True) 
+Path(buildpath).mkdir(exist_ok=True)
 os.chdir(buildpath)
 os.system("pwd")
 for run_num in range(len(monors)*len(paramslist)):
@@ -104,14 +107,14 @@ for run_num in range(len(monors)*len(paramslist)):
 os.system("make clean && make -j 16 cond0D")
 
 # 2a. create file with gridbox boundaries
-Path(binpath).mkdir(parents=True, exist_ok=True)             
+Path(binpath).mkdir(parents=True, exist_ok=True)
 os.system("rm "+gridfile)
-cgrid.write_gridboxboundaries_binary(gridfile, zgrid, xgrid, 
-                                                     ygrid, constsfile)
+cgrid.write_gridboxboundaries_binary(gridfile, zgrid, xgrid,
+                                     ygrid, constsfile)
 rgrid.print_domain_info(constsfile, gridfile)
 if isfigures[0]:
-    rgrid.plot_gridboxboundaries(constsfile, gridfile, 
-                                              binpath, isfigures[1])
+    rgrid.plot_gridboxboundaries(constsfile, gridfile,
+                                 binpath, isfigures[1])
 plt.close()
 
 runnum = 0
@@ -119,21 +122,23 @@ for i in range(len(monors)):
 
     # 2b. create file with initial SDs conditions
     monor, numconc = monors[i], numconcs[i]
-    radiigen  = initattributes.MonoAttrsGen(monor)       # all SDs have the same dryradius = monor [m]
-    radiiprobdist = radiiprobdistribs.DiracDelta(monor)  # monodisperse droplet radii probability distribution
+    # all SDs have the same dryradius = monor [m]
+    radiigen = initattributes.MonoAttrsGen(monor)
+    # monodisperse droplet radii probability distribution
+    radiiprobdist = radiiprobdistribs.DiracDelta(monor)
 
-    initattrsgen = initattributes.InitManyAttrsGen(radiigen,radiiprobdist,
+    initattrsgen = initattributes.InitManyAttrsGen(radiigen, radiiprobdist,
                                                    coord3gen, coord1gen, coord2gen)
     os.system("rm "+initSDsfile)
-    create_initsuperdrops.write_initsuperdrops_binary(initSDsfile, initattrsgen, 
+    create_initsuperdrops.write_initsuperdrops_binary(initSDsfile, initattrsgen,
                                                       configfile, constsfile,
                                                       gridfile, nsupers, numconc)
 
     if isfigures[0]:
-        read_initsuperdrops.plot_initdistribs(configfile, constsfile, initSDsfile,
-                                                gridfile, binpath, isfigures[1])
+        read_initsuperdrops.plot_initGBxsdistribs(configfile, constsfile, initSDsfile,
+                                              gridfile, binpath, isfigures[1], "all")
         plt.close()
-    
+
     fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(5, 16))
     for params in paramslist:
 
@@ -146,45 +151,45 @@ for i in range(len(monors)):
 
         # 4. run model
         os.system(buildpath+'src/cond0D ' +
-                   configfile+' '+constsfile)
+                  configfile+' '+constsfile)
 
         # 5. load results
         setupfile = binpath+"arabassetup.txt"
         dataset = binpath+"arabassol"+str(runnum)+".zarr"
 
         # read in constants and intial setup from setup .txt file
-        setup, grid = pysetuptxt.get_setup_grid(setupfile, gridfile) 
+        setup, grid = pysetuptxt.get_setup_gridinfo(setupfile, gridfile)
         SDprops = sdprops.CommonSuperdropProperties(setup["RHO_L"], setup["RHO_SOL"],
-                                                              setup["MR_SOL"], setup["IONIC"])
+                                                    setup["MR_SOL"], setup["IONIC"])
         thermo = pyzarr.get_thermodata(dataset, setup, grid["ndims"])
         time = pyzarr.get_time(dataset).secs
         sddata = pyzarr.get_sddata(dataset)
         zprof = displacement(time, setup["W_AVG"], setup["T_HALF"])
-       
+
         radius = pyzarr.attrtimeseries_for_1superdrop(sddata, 0, "radius")
         eps = pyzarr.attrtimeseries_for_1superdrop(sddata, 0, "eps")
         m_sol = pyzarr.attrtimeseries_for_1superdrop(sddata, 0, "m_sol")
 
-        numconc = np.sum(sddata["eps"][0])/grid["domainvol"]/1e6 # [/cm^3]
+        numconc = np.sum(sddata["eps"][0])/grid["domainvol"]/1e6  # [/cm^3]
 
         # relative humidty and supersaturation
-        press = thermo.press*100 #convert from hPa to Pa
-        relh, supersat = thermoeqns.relative_humidity(press, thermo.temp, 
-                                              thermo.qvap, setup["Mr_ratio"])
-                                              
+        press = thermo.press*100  # convert from hPa to Pa
+        relh, supersat = thermoeqns.relative_humidity(press, thermo.temp,
+                                                      thermo.qvap, setup["Mr_ratio"])
+
         # 5. plot results
         wlab = "<w> = {:.1f}".format(float(setup["W_AVG"])*100)+"cm s$^{-1}$"
         axs = ccs.condensation_validation_subplots(axs, time, radius,
-                                                    supersat.flatten(),
-                                                    zprof,
-                                                    lwdth=params["lwdth"],
-                                                    lab=wlab)
+                                                   supersat.flatten(),
+                                                   zprof,
+                                                   lwdth=params["lwdth"],
+                                                   lab=wlab)
 
         runnum += 1
 
     ccs.plot_kohlercurve_with_criticalpoints(axs[1], radius, m_sol[0],
-                                                thermo.temp.flatten()[0],
-                                                SDprops.IONIC, SDprops.MR_SOL)
+                                             thermo.temp.flatten()[0],
+                                             SDprops.IONIC, SDprops.MR_SOL)
 
     textlab = "N = "+str(numconc)+"cm$^{-3}$\n" +\
               "r$_{dry}$ = "+"{:.2g}\u03BCm\n".format(radius[0])
@@ -203,7 +208,7 @@ for i in range(len(monors)):
     fig.tight_layout()
 
     savename = "arabas_shima_condensation_"+str(i)+".png"
-    fig.savefig(binpath+savename, dpi=400, 
+    fig.savefig(binpath+savename, dpi=400,
                 bbox_inches="tight", facecolor='w', format="png")
     print("Figure .png saved as: "+binpath+savename)
-    plt.show()      
+    plt.show()
