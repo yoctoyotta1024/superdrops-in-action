f2c6e010f5d09cabf987be2820c30bc109974944
clara.bayley <clara.bayley@mpimet.mpg.de>
2025-07-14 12:06:04 +0200

    feat: empty repository necessities

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""
+Copyright (c) 2025 MPI-M, Clara Bayley
+
+----- superdrops-in-action -----
+File: setup.py
+Project: superdrops-in-action
+Created Date: Monday 14th July 2025
+Author: Clara Bayley (CB)
+Additional Contributors:
+-----
+Last Modified: Monday 14th July 2025
+Modified By: CB
+-----
+License: BSD 3-Clause "New" or "Revised" License
+https://opensource.org/licenses/BSD-3-Clause
+-----
+File Description:
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="superdrops-in-action",
+    version="X.Y.Z",
+    packages=find_packages(),
+    install_requires=[
+        "pytest",
+        "sphinx",
+    ],
+)
