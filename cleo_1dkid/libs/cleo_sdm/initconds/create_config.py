a30c6e80180c4cc1ed162eaa386accf7767145fb
clara.bayley <clara.bayley@mpimet.mpg.de>
2025-07-23 19:47:43 +0200

    feat: use bash script to create configs for each run

--- cleo_1dkid/libs/cleo_sdm/initconds/create_config.py
+++ cleo_1dkid/libs/cleo_sdm/initconds/create_config.py
@@ -20,141 +20,78 @@ import sys
 import shutil
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).parent.parent))  # scripts directory
-import shared_script_variables as ssv
-
 parser = argparse.ArgumentParser()
 parser.add_argument("path2CLEO", type=Path, help="Absolute path to CLEO (for pySD)")
-parser.add_argument("path2builds", type=Path, help="Absolute path to builds")
 parser.add_argument(
-    "buildtype",
-    type=str,
-    choices=["serial", "openmp", "cuda", "threads"],
-    help="Type of build: serial, openmp, cuda or threads",
+    "src_config_filename", type=Path, help="Absolute path to original config"
 )
 parser.add_argument(
-    "--gen_initconds",
-    type=str,
-    choices=["TRUE", "FALSE"],
-    default="TRUE",
-    help="=='TRUE', else don't generate initial condition binary files",
+    "dest_config_filename", type=Path, help="Absolute path to destination config"
+)
+parser.add_argument(
+    "--cleoconstants_filepath",
+    type=Path,
+    default="NONE",
+    help="path to cleoconstants.hpp file",
+)
+parser.add_argument(
+    "--grid_filename",
+    type=Path,
+    default="NONE",
+    help="path to gridbox binary file",
+)
+parser.add_argument(
+    "--initsupers_filename",
+    type=Path,
+    default="NONE",
+    help="path to initial superdroplets binary file",
+)
+parser.add_argument(
+    "--setup_filename",
+    type=Path,
+    default="NONE",
+    help="path to output .txt file",
+)
+parser.add_argument(
+    "--zarrbasedir",
+    type=Path,
+    default="NONE",
+    help="path to output .zarr directory",
 )
 args = parser.parse_args()
 
-path2src = Path(__file__).resolve().parent.parent.parent / "src"
-path2CLEO = args.path2CLEO
-path2builds = args.path2builds
-buildtype = args.buildtype
-if args.gen_initconds == "TRUE":
-    gen_initconds = True
-else:
-    gen_initconds = False
-
-sys.path.append(str(path2CLEO))  # for imports for editing a config file
-sys.path.append(str(path2src))  # for imports for input files generation
-from collisions0d import initconds_colls0d as initconds
+sys.path.append(str(args.path2CLEO))  # for pySD import for editing a config file
 from pySD import editconfigfile
 
 ### ----- create temporary config file for simulation(s) ----- ###
-src_config_filename = path2src / "collisions0d" / "config_colls0d.yaml"
+cleoconstants_filepath = args.cleoconstants_filepath
+grid_filename = args.grid_filename
+initsupers_filename = args.initsupers_filename
+setup_filename = args.setup_filename
+zarrbasedir = args.zarrbasedir
 
-ngbxs_nsupers_runs = ssv.get_ngbxs_nsupers_runs()
-ngbxs_nsupers_nthreads = ssv.get_ngbxs_nsupers_nthreads(
-    buildtype, ngbxs_nsupers_runs=ngbxs_nsupers_runs
-)
+# check directories meet requirements
+assert cleoconstants_filepath.parent.is_dir()
+assert grid_filename.parent.is_dir()
+assert initsupers_filename.parent.is_dir()
+assert setup_filename.parent.is_dir()
+assert zarrbasedir.parent.is_dir()
+assert setup_filename.suffix == ".txt"
+assert zarrbasedir.suffix == ".zarr"
 
-savefigpath = path2builds / "bin" / "colls0d"
-sharepath = path2builds / "share" / "colls0d"
-binpath = path2builds / buildtype / "bin" / "colls0d"
-tmppath = path2builds / buildtype / "tmp" / "colls0d"
-constants_filepath = path2builds / buildtype / "_deps" / "cleo-src" / "libs"
+# copy src_config to dest_config and then edit parameters in this dictionary
 params = {
-    "constants_filename": str(constants_filepath / "cleoconstants.hpp"),
-    "sharepath": str(sharepath),
-    "savefigpath": str(savefigpath),
+    "constants_filename": str(cleoconstants_filepath / "cleoconstants.hpp"),
+    "grid_filename": str(grid_filename),
+    "initsupers_filename": str(initsupers_filename),
+    "setup_filename": str(setup_filename),
+    "zarrbasedir": str(zarrbasedir),
 }
 
-
-def get_grid_filename(sharepath: Path, ngbxs: int):
-    return str(sharepath / f"dimlessGBxboundaries_{ngbxs}.dat")
-
-
-def get_initsupers_filename(sharepath: Path, ngbxs: int, nsupers: int, nrun: int):
-    return sharepath / f"dimlessSDsinit_{ngbxs}_{nsupers}_{nrun}.dat"
-
-
-### --- ensure build, share and bin directories exist --- ###
-if path2CLEO == path2builds:
-    raise ValueError("build directory cannot be CLEO")
-else:
-    savefigpath.mkdir(exist_ok=True, parents=True)
-    sharepath.mkdir(exist_ok=True, parents=True)
-    binpath.mkdir(exist_ok=True, parents=True)
-    tmppath.mkdir(exist_ok=True, parents=True)
-
-for ngbxs, nsupers in ngbxs_nsupers_runs.keys():
-    ### ----- Copy config to temporary file and edit specific parameters ----- ###
-    for nrun in range(ngbxs_nsupers_runs[(ngbxs, nsupers)]):
-        for nthreads in ngbxs_nsupers_nthreads[(ngbxs, nsupers)]:
-            config_filename = ssv.get_config_filename(
-                tmppath, ngbxs, nsupers, nrun, nthreads=nthreads
-            )
-            binpath_run = ssv.get_run_binpath(
-                binpath, ngbxs, nsupers, nrun, nthreads=nthreads
-            )
-
-            params["num_threads"] = nthreads
-
-            params["ngbxs"] = ngbxs
-            params["grid_filename"] = str(get_grid_filename(sharepath, ngbxs))
-            if ngbxs < 64:
-                ndim_x = ndim_y = 1
-            else:
-                ndim_x = ndim_y = 8
-                assert ngbxs % 64 == 0, "ngbxs > 64 must also be a multiple of 64"
-            ndim_z = ngbxs / ndim_x / ndim_y
-            assert (
-                ndim_x * ndim_z * ndim_y == ngbxs
-            ), "product of ndims must equal ngbxs"
-            params["zgrid"] = [0, 10000, 10000 / ndim_z]
-            params["xgrid"] = [0, 10000, 10000 / ndim_x]
-            params["ygrid"] = [0, 10000, 10000 / ndim_y]
-
-            params["maxnsupers"] = nsupers * ngbxs
-            params["initsupers_filename"] = str(
-                get_initsupers_filename(sharepath, ngbxs, nsupers, nrun)
-            )
-
-            params["setup_filename"] = str(binpath_run / "setup.txt")
-            params["zarrbasedir"] = str(binpath_run / "sol.zarr")
-
-            shutil.copy(Path(src_config_filename), config_filename)
-            editconfigfile.edit_config_params(config_filename, params)
-
-if gen_initconds:
-    for ngbxs, nsupers in ngbxs_nsupers_runs.keys():
-        isfigures = [True, True]
-        nthreads_dummy = ngbxs_nsupers_nthreads[(ngbxs, nsupers)][
-            0
-        ]  # all nthreads use same initial conditions
-        ### ----- write initial gridbox boundaries binary file ----- ###
-        nrun_dummy = 0  # all runs use same init gbxs
-        config_filename = ssv.get_config_filename(
-            tmppath, ngbxs, nsupers, nrun=nrun_dummy, nthreads=nthreads_dummy
-        )
-        shutil.rmtree(get_grid_filename(sharepath, ngbxs), ignore_errors=True)
-        initconds.gridbox_boundaries(path2CLEO, config_filename, isfigures=isfigures)
-
-        ### ----- write initial superdroplets binary files ----- ###
-        for nrun in range(ngbxs_nsupers_runs[(ngbxs, nsupers)]):
-            config_filename = ssv.get_config_filename(
-                tmppath, ngbxs, nsupers, nrun, nthreads=nthreads_dummy
-            )
-            shutil.rmtree(
-                get_initsupers_filename(sharepath, ngbxs, nsupers, nrun),
-                ignore_errors=True,
-            )
-            initconds.initial_superdroplet_conditions(
-                path2CLEO, config_filename, isfigures=isfigures
-            )
-            isfigures = [False, False]  # only plot SD figures once
+print("--- create_config configuration arguments ---")
+print(args.src_config_filename, args.dest_config_filename)
+for k, v in params.items():
+    print(k, v)
+print("---------------------------------------------")
+shutil.copy(args.src_config_filename, args.dest_config_filename)
+editconfigfile.edit_config_params(args.dest_config_filename, params)
--- cleo_1dkid/scripts/bash/inputfiles_cleo_1dkid.sh
+++ cleo_1dkid/scripts/bash/inputfiles_cleo_1dkid.sh
@@ -15,31 +15,69 @@
 ### ----- You need to edit these lines to set your ----- ###
 ### ----- default compiler and python environment   ---- ###
 ### ----  and paths for CLEO and build directories  ---- ###
+path2build=$1 # should be abolute path
 path2CLEO=${HOME}/CLEO
-path2scripts=${HOME}/superdrops-in-action/cleo_1dkid/libs/cleo_sdm/initconds
 python=/work/bm1183/m300950/bin/envs/superdrops-in-action/bin/python
+path2cleo1dkid=${HOME}/superdrops-in-action/cleo_1dkid
+path2scripts=${path2cleo1dkid}/libs/cleo_sdm/initconds
+
+cleoconstants_filepath="${path2cleo1dkid}/cleo_deps/libs/"
+grid_filename="${path2build}/share/dimlessGBxboundaries.dat"
+initsupers_filename="${path2build}/share/dimlessSDsinit.dat"
+
+### srcs and dests are lists of absolute paths to source and destination config files
+### seperated by spaces e.g. srcs=("$HOME/config1" "$HOME/config2")
+srcs=("${path2cleo1dkid}/share/cleo_initial_conditions/1dkid/condevap_only/config.yaml"
+      "${path2cleo1dkid}/share/cleo_initial_conditions/1dkid/fullscheme/config.yaml")
+dests=("${path2build}/tmp/condevap_only/config.yaml"
+       "${path2build}/tmp/fullscheme/config.yaml")
+setup_filenames=("${path2build}/bin/condevap_only/setup.txt"
+                 "${path2build}/bin/fullscheme/setup.txt")
+zarrbasedirs=("${path2build}/bin/condevap_only/sol.zarr"
+              "${path2build}/bin/fullscheme/sol.zarr")
 ### ---------------------------------------------------- ###
 
-path2build=$1 # should be abolute path
-configfiles=("${@:2}") # list of absolute paths separated by spaces with "", e.g. "$HOME/config0.yaml $HOME/config1.yaml"
 
-if [[ "${configfiles}" == "" || "${path2build}" == "" ]]
+if [[ ${#srcs[@]} -eq 0 || "${path2build}" == "" ]]
 then
-  echo "Please specify config files and path2build"
-else
-  for configfile in ${configfiles};
-  do
-    echo "config file: ${configfile}"
-    echo "path to build directory: ${path2build}"
-
-    ### --------------- create gbx boundaries -------------- ###
-    echo "${python} create_gbxboundariesbinary_script.py ${path2CLEO} ${path2build} ${configfile}"
-    ${python} ${path2scripts}/create_gbxboundariesbinary_script.py ${path2CLEO} ${path2build} ${configfile}
-    ### ---------------------------------------------------- ###
-
-    ### -------- create superdrop initial conditions ------- ###
-    echo "${python} create_initsuperdropsbinary_script.py ${path2CLEO} ${path2build} ${configfile}"
-    ${python} ${path2scripts}/create_initsuperdropsbinary_script.py ${path2CLEO} ${path2build} ${configfile}
-    ### ---------------------------------------------------- ###
-  done
+  echo "Please specify path2build and source and destination config files"
+  exit 1
 fi
+
+if [[ ${#srcs[@]} -ne ${#dests[@]} || ${#srcs[@]} -ne ${#setup_filenames[@]} || ${#srcs[@]} -ne ${#zarrbasedirs[@]} ]]
+then
+  echo "Please specify as many source configuration files as destinations"
+  echo "(${#srcs[@]}, ${#dests[@]}, ${#setup_filenames[@]}, ${#zarrbasedirs[@]})"
+  exit 1
+fi
+
+for i in "${!srcs[@]}"
+do
+  src_configfile="${srcs[i]}"
+  dest_configfile="${dests[i]}"
+  setup_filename="${setup_filenames[i]}"
+  zarrbasedir="${zarrbasedirs[i]}"
+  echo "path to build directory: ${path2build}"
+  echo "src config file: ${src_configfile}"
+  echo "dest config file: ${dest_configfile}"
+
+  ### --------------- create configuration file -------------- ###
+  echo "${python} create_config.py ${path2CLEO} ${src_configfile} ${dest_configfile} [...]"
+  ${python} ${path2scripts}/create_config.py ${path2CLEO} ${src_configfile} ${dest_configfile} \
+    --cleoconstants_filepath="${cleoconstants_filepath}" \
+    --grid_filename="${grid_filename}" \
+    --initsupers_filename="${initsupers_filename}" \
+    --setup_filename="${setup_filename}" \
+    --zarrbasedir="${zarrbasedir}"
+  ### ---------------------------------------------------- ###
+
+  ### --------------- create gbx boundaries -------------- ###
+  echo "${python} create_gbxboundariesbinary_script.py ${path2CLEO} ${path2build} ${dest_configfile}"
+  ${python} ${path2scripts}/create_gbxboundariesbinary_script.py ${path2CLEO} ${path2build} ${dest_configfile}
+  ### ---------------------------------------------------- ###
+
+  ### -------- create superdrop initial conditions ------- ###
+  echo "${python} create_initsuperdropsbinary_script.py ${path2CLEO} ${path2build} ${dest_configfile}"
+  ${python} ${path2scripts}/create_initsuperdropsbinary_script.py ${path2CLEO} ${path2build} ${dest_configfile}
+  ### ---------------------------------------------------- ###
+done
