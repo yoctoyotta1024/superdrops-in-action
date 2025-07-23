c2ed388752d8e2c1122ef473063bceb792b7f1aa
clara.bayley <clara.bayley@mpimet.mpg.de>
2025-07-23 17:33:48 +0200

    chore: remove last modified from file headers

--- cleo_1dkid/scripts/run_cleo_1dkid_fullscheme.py
+++ cleo_1dkid/scripts/run_cleo_1dkid_fullscheme.py
@@ -8,9 +8,6 @@ Created Date: Monday 14th July 2025
 Author: Clara Bayley (CB)
 Additional Contributors:
 -----
-Last Modified: Monday 14th July 2025
-Modified By: CB
------
 License: BSD 3-Clause "New" or "Revised" License
 https://opensource.org/licenses/BSD-3-Clause
 -----
