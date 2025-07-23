12695acccb0a930bf4d120c8c054506c01846554
clara.bayley <clara.bayley@mpimet.mpg.de>
2025-07-23 17:27:42 +0200

    chore: copy changes from CLEO 0.52.0 superdrops library into cleo_deps

--- cleo_1dkid/cleo_deps/libs/superdrops/sdmmonitor.hpp
+++ cleo_1dkid/cleo_deps/libs/superdrops/sdmmonitor.hpp
@@ -9,9 +9,6 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Friday 21st June 2024
- * Modified By: CB
- * -----
  * License: BSD 3-Clause "New" or "Revised" License
  * https://opensource.org/licenses/BSD-3-Clause
  * -----
@@ -25,11 +22,18 @@
 #include <concepts>
 
 #include "kokkosaliases_sd.hpp"
+#include "state.hpp"
+#include "superdrop.hpp"
 
 /**
  * @brief Concept of SDMmonitor to monitor various SDM processes.
  *
- * _Note:_ Constraints missing `{ mo.monitor_motion(d_gbxs, domainsupers) } -> std::same_as<void>;`
+ * _Note:_ More exact contraint missing:
+ * `{ mo.monitor_motion(d_gbxs, domainsupers) } -> std::same_as<void>;`
+ * to avoid adding constraint over templated argument types.
+ *
+ * _Note:_ More exact contraint missing:
+ * `{ mo.monitor_precipitation(unsigned int, gbxmaps, state, superdrop) } -> std::same_as<void>;`
  * to avoid adding constraint over templated argument types.
  *
  * @tparam SDMMo Type that satisfies the SDMMonitor concept.
@@ -38,8 +42,11 @@ template <typename SDMMo>
 concept SDMMonitor =
     requires(SDMMo mo, const TeamMember &tm, const double d, const viewd_constsupers supers) {
       { mo.reset_monitor() } -> std::same_as<void>;
+      { mo.before_timestepping(tm, supers) } -> std::same_as<void>;
       { mo.monitor_condensation(tm, d) } -> std::same_as<void>;
       { mo.monitor_microphysics(tm, supers) } -> std::same_as<void>;
+      // { mo.monitor_motion };
+      // { mo.monitor_precipitation };
     };
 
 /**
@@ -80,6 +87,17 @@ struct CombinedSDMMonitor {
    * Each monitor is run sequentially.
    */
   KOKKOS_FUNCTION
+  void before_timestepping(const TeamMember &tm, const viewd_constsupers supers) const {
+    a.before_timestepping(tm, supers);
+    b.before_timestepping(tm, supers);
+  }
+
+  /**
+   * @brief monitor microphysics for combination of 2 sdm monitors.
+   *
+   * Each monitor is run sequentially.
+   */
+  KOKKOS_FUNCTION
   void monitor_condensation(const TeamMember &tm, const double d) const {
     a.monitor_condensation(tm, d);
     b.monitor_condensation(tm, d);
@@ -105,6 +123,18 @@ struct CombinedSDMMonitor {
     a.monitor_motion(d_gbxs, domainsupers);
     b.monitor_motion(d_gbxs, domainsupers);
   }
+
+  /**
+   * @brief monitor motion for combination of 2 sdm monitors.
+   *
+   * Each monitor is run sequentially.
+   */
+  KOKKOS_FUNCTION
+  void monitor_precipitation(const TeamMember &team_member, const unsigned int gbxindex,
+                             const auto &gbxmaps, Superdrop &drop) const {
+    a.monitor_precipitation(team_member, gbxindex, gbxmaps, drop);
+    b.monitor_precipitation(team_member, gbxindex, gbxmaps, drop);
+  }
 };
 
 /**
@@ -116,12 +146,19 @@ struct NullSDMMonitor {
   void reset_monitor() const {}
 
   KOKKOS_FUNCTION
+  void before_timestepping(const TeamMember &team_member, const viewd_constsupers supers) const {}
+
+  KOKKOS_FUNCTION
   void monitor_condensation(const TeamMember &team_member, const double d) const {}
 
   KOKKOS_FUNCTION
   void monitor_microphysics(const TeamMember &team_member, const viewd_constsupers supers) const {}
 
   void monitor_motion(const auto d_gbxs, const auto domainsupers) const {}
+
+  KOKKOS_FUNCTION
+  void monitor_precipitation(const TeamMember &team_member, const unsigned int gbxindex,
+                             const auto &gbxmaps, Superdrop &drop) const {}
 };
 
-#endif  //  CLEO_1DKID_CLEO_DEPS_LIBS_SUPERDROPS_SDMMONITOR_HPP_
+#endif  // CLEO_1DKID_CLEO_DEPS_LIBS_SUPERDROPS_SDMMONITOR_HPP_
