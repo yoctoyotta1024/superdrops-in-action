d1822972f9eeafc28272ac2e1f1f92d8eac01af5
clara.bayley <clara.bayley@mpimet.mpg.de>
2025-07-25 18:14:01 +0200

    refactor: add hill plots for ensemble

--- cleo_1dkid/scripts/plot_cleo_1dkid_ensemble.py
+++ cleo_1dkid/scripts/plot_cleo_1dkid_ensemble.py
@@ -83,6 +83,21 @@ def plot_vertical_error_shading(
         ax.plot(mean, y, **mean_kwargs)
 
 
+def plot_horizontal_error_shading(
+    ax,
+    x,
+    mean,
+    lower_error,
+    upper_error,
+    plot_mean=False,
+    shading_kwargs={"alpha": 0.3, "color": "pink"},
+    mean_kwargs={"color": "black"},
+):
+    ax.fill_between(x, mean + lower_error, mean + upper_error, **shading_kwargs)
+    if plot_mean:
+        ax.plot(x, mean, **mean_kwargs)
+
+
 # %%
 assert args.binpath.is_dir()
 setupfiles = glob.glob(os.path.join(args.binpath, "*.txt"))
@@ -100,10 +115,41 @@ time = pyzarr.get_time(datasets[0])
 ds = xr.open_mfdataset(datasets, engine="zarr", combine="nested", concat_dim="ensemble")
 ensemble_coord = dict(ensemble=("ensemble", [str(Path(d).stem) for d in datasets]))
 ds = ds.assign_coords(ensemble_coord)
+
 ds = ds.rename_dims({"gbxindex": "height"})
 ds = ds.drop_vars("gbxindex")
 ds = ds.assign_coords(height=("height", gbxs["zfull"]))
 ds["height"].attrs["units"] = "m"
+
+arr = xr.DataArray(
+    gbxs["gbxvols"][0, 0, :], name="volume", dims="height", attrs={"units": "m^3"}
+)
+ds = ds.assign(**{arr.name: arr})
+
+arr = xr.DataArray(
+    ds.massmom0 / ds.volume / 1e6,
+    name="numconc",
+    dims=["ensemble", "time", "height"],
+    attrs={"units": "cm^-3"},
+)
+ds = ds.assign(**{arr.name: arr})
+
+arr = xr.DataArray(
+    ds.massmom1 / ds.volume,
+    name="lwc",
+    dims=["ensemble", "time", "height"],
+    attrs={"units": "g m^-3"},
+)
+ds = ds.assign(**{arr.name: arr})
+
+arr = xr.DataArray(
+    ds.lwc.integrate(coord="height") / 1000,
+    name="lwp",
+    dims=["ensemble", "time"],
+    attrs={"units": "Kg m^-2"},
+)
+ds = ds.assign(**{arr.name: arr})
+
 ds
 
 
@@ -184,4 +230,91 @@ times4xsection = {  # s: color
 plot_qvap_qcond(ds, times4xsection)
 plt.show()
 
+
 # %%
+def plot_hill_figure2(ds):
+    fig, axs = plt.subplots(nrows=4, ncols=1, figsize=(5, 10), sharey=True)
+
+    nc0 = ds.numconc.sel(time=0, method="nearest").mean().values
+    fig.suptitle("Fig. 2, N$_a$ = {:.0f} ".format(nc0) + "cm$^{-3}$")
+
+    lwc_10mins = ds.lwc.sel(time=600, method="nearest")
+    mean, err1, err2 = mean_stddev(lwc_10mins, dim="ensemble")
+    plot_vertical_error_shading(
+        axs[0],
+        ds.height,
+        mean,
+        err1,
+        err2,
+        plot_mean=True,
+        shading_kwargs={"color": "blue", "alpha": 0.3},
+        mean_kwargs={
+            "color": "blue",
+            "label": "time={:.0f}mins".format(lwc_10mins.time.values / 60),
+        },
+    )
+    axs[0].legend()
+    axs[0].set_xlabel("LWC / g m$^{-3}$")
+
+    axs[0].set_ylim([0, 3000])
+    for ax in axs:
+        ax.set_ylabel("height / m")
+
+    fig.tight_layout()
+
+
+plot_hill_figure2(ds)
+plt.show()
+
+
+# %%
+def plot_lwc_max_height(ds):
+    fig, axs = plt.subplots(nrows=2, ncols=1)
+
+    maxlwc = ds.lwc.max(dim="height")
+    height_maxlwc = ds.lwc.idxmax(dim="height")
+
+    ds.lwc.sel(ensemble="sol_0").T.plot(ax=axs[0])
+    height_maxlwc.sel(ensemble="sol_0").plot(ax=axs[0], color="grey")
+
+    ds.lwc.sel(height=height_maxlwc).sel(ensemble="sol_0").plot(ax=axs[1])
+    maxlwc.sel(ensemble="sol_0").plot(linestyle="--", ax=axs[1], color="grey")
+
+    fig.tight_layout()
+
+
+plot_lwc_max_height(ds)
+plt.show
+
+
+# %%
+def plot_hill_figure2(ds):
+    fig, axs = plt.subplots(nrows=5, ncols=1, figsize=(5, 10), sharex=True)
+
+    nc0 = ds.numconc.sel(time=0, method="nearest").mean().values
+    fig.suptitle("Fig. 4, N$_a$ = {:.0f} ".format(nc0) + "cm$^{-3}$")
+
+    mean, err1, err2 = mean_stddev(ds.lwp, dim="ensemble")
+    plot_horizontal_error_shading(
+        axs[0],
+        ds.time,
+        mean,
+        err1,
+        err2,
+        shading_kwargs={"alpha": 0.3, "color": "pink"},
+        mean_kwargs={"color": "pink"},
+        plot_mean=True,
+    )
+    axs[0].set_ylabel("LWP / kg m$^{-2}$")
+    axs[0].set_ylim(bottom=0)
+
+    # height_maxlwc = ds.lwc.idxmax(dim="height")
+
+    axs[0].set_xlim([60, 3600])
+    axs[-1].set_xlabel("time / s")
+
+    fig.tight_layout()
+
+
+plot_hill_figure2(ds)
+plt.show()
