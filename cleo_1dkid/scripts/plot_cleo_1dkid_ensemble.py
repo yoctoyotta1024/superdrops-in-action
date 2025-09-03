e3447415ff9dcbd39ae2243ddfa86512277c330f
clara.bayley <clara.bayley@mpimet.mpg.de>
2025-09-03 17:34:47 +0200

    feat: plot figure 2 of hill paper

--- cleo_1dkid/scripts/plot_cleo_1dkid_ensemble.py
+++ cleo_1dkid/scripts/plot_cleo_1dkid_ensemble.py
@@ -23,6 +23,8 @@ import argparse
 import glob
 import os
 import sys
+import awkward as ak
+import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 from pathlib import Path
@@ -62,6 +64,139 @@ from pySD.sdmout_src import pyzarr, pysetuptxt, pygbxsdat  # from plotssrc impor
 
 
 # %%
+def unflatten_superdrops(rawdata, raggedcount, nsupers):
+    sdarr = ak.unflatten(rawdata, raggedcount)
+    sdarr = ak.to_regular(ak.unflatten(sdarr, ak.flatten(nsupers), axis=1), axis=1)
+    return sdarr
+
+
+def superdrops_variable_ensemble(ds, var):
+    data = {
+        e: unflatten_superdrops(
+            ds.sel(ensemble=e)[var].values,
+            ds.sel(ensemble=e).raggedcount.values,
+            ds.sel(ensemble=e).nsupers.values,
+        )
+        for e in ds.ensemble.values
+    }
+    return data
+
+
+def incloud_numconc(ds, xi, radius, assign_to_dataset=False):
+    numconc_d = {}
+    for e in ds.ensemble.values:
+        mask_incloud_lwc = (ds.sel(ensemble=e).lwc / 1000 > 1e-5).values
+        v = ak.where(mask_incloud_lwc[:, :, None], xi[e], 0.0)
+        xi_incloud = ak.where(radius[e] > 1, v, 0.0)
+
+        sum_xi_incloud = ak.to_numpy(ak.sum(xi_incloud, axis=2), allow_missing=False)
+        numconc_d[e] = sum_xi_incloud / ds.volume.values[None, :] / 1e6
+
+    numconc_d = xr.DataArray(
+        list(numconc_d.values()),
+        name="numconc_d",
+        dims=["ensemble", "time", "height"],
+        attrs={"units": "cm^-3"},
+    )
+
+    if assign_to_dataset:
+        ds = ds.assign(**{numconc_d.name: numconc_d})
+
+    return ds, numconc_d
+
+
+def mass_water(radius, msol, RHO_L, RHO_SOL):
+    msol_kg = msol / 1000  # convert msol from grams to Kg
+    radius_m = radius / 1e6  # convert microns to m
+
+    v_sol = msol_kg / RHO_SOL
+    v_w = 4 / 3.0 * np.pi * (radius_m**3) - v_sol
+
+    m_water = RHO_L * v_w
+    m_water_corrected = np.where(m_water <= 0.0, 0.0, m_water)
+
+    return m_water_corrected  # [kg]
+
+
+def volume_diameter(xi, m_water, RHO_L):
+    """xi and m_water here are single member of ensemble not entire dictionary"""
+    weighted_water = ak.to_numpy(
+        ak.sum((m_water) ** (4 / 3) * xi, axis=2), allow_missing=False
+    )
+    sum_water = ak.to_numpy(ak.sum(m_water * xi, axis=2), allow_missing=False)
+
+    dvol = (6.0 / np.pi / RHO_L) ** (1 / 3) * weighted_water / sum_water
+
+    return dvol
+
+
+def mass_weighted_mean_volume_diameter(ds, xi, m_water, RHO_L, assign_to_dataset=False):
+    dvol = {}
+    for e in ds.ensemble.values:
+        dvol[e] = volume_diameter(xi[e], m_water[e], RHO_L) * 1e6  # [microns]
+
+    dvol = xr.DataArray(
+        list(dvol.values()),
+        name="dvol",
+        dims=["ensemble", "time", "height"],
+        attrs={"units": "micro-m", "long_name": "mass weighted mean volume diameter"},
+    )
+
+    if assign_to_dataset:
+        ds = ds.assign(**{dvol.name: dvol})
+
+    return ds, dvol
+
+
+def volume_diameter_stddev(xi, m_water, dvol, RHO_L):
+    """xi and m_water here are single member of ensemble not entire dictionary"""
+
+    sumxi = ak.to_numpy(ak.sum(xi, axis=2), allow_missing=False)
+    di = (6.0 / np.pi / RHO_L * m_water) ** (1 / 3)
+    diffssqrd = xi * (di - dvol.values[:, :, None]) ** 2
+    sumdiffssqrd = ak.to_numpy(ak.sum(diffssqrd, axis=2), allow_missing=False)
+
+    sigma = (1 / (sumxi - 1) * sumdiffssqrd) ** (1 / 2)
+
+    return sigma
+
+
+def mass_weighted_volume_diameter_stddev(
+    ds, xi, m_water, RHO_L, assign_to_dataset=False
+):
+    sigma = {}
+    for e in ds.ensemble.values:
+        dvol_m = ds.sel(ensemble=e).dvol / 1e6
+        sigma[e] = (
+            volume_diameter_stddev(xi[e], m_water[e], dvol_m, RHO_L) * 1e6
+        )  # [microns]
+
+    sigma = xr.DataArray(
+        list(sigma.values()),
+        name="dvol_sigma",
+        dims=["ensemble", "time", "height"],
+        attrs={
+            "units": "micro-m",
+            "long_name": "standard deviation of mass weighted volume diameter",
+        },
+    )
+
+    if assign_to_dataset:
+        ds = ds.assign(**{sigma.name: sigma})
+
+    return ds, sigma
+
+
+# %%
+def rho(press, temp, qvap):
+    p = press * 100  # [Pa]
+    qv = qvap / 1000  # [kg/kg]
+    Rd = 287.0027
+    Rv = 461.52998157941937
+    Rq = (Rv * qv + Rd) / (1 + qv)
+    return p / Rq / temp
+
+
 def mean_stddev(arr, dim="ensemble"):
     """return mean +- stddev over dimension of array (default over ensemble)"""
     mean = arr.mean(dim=dim)
@@ -97,6 +232,13 @@ def plot_horizontal_error_shading(
     ax.fill_between(x, mean + lower_error, mean + upper_error, **shading_kwargs)
     if plot_mean:
         ax.plot(x, mean, **mean_kwargs)
+
+
+def save_figure(fig, savename):
+    if savename.parent.is_dir():
+        fig.savefig(savename, dpi=800, bbox_inches="tight", facecolor="w")
+    else:
+        print("not saving figure, no existing directory provided")
 
 
 # %%
@@ -152,16 +294,22 @@ arr = xr.DataArray(
 ds = ds.assign(**{arr.name: arr})
 
 ds
-
-
-# %%
-def rho(press, temp, qvap):
-    p = press * 100  # [Pa]
-    qv = qvap / 1000  # [kg/kg]
-    Rd = 287.0027
-    Rv = 461.52998157941937
-    Rq = (Rv * qv + Rd) / (1 + qv)
-    return p / Rq / temp
+# %% (slow) superdroplet variables across ensemble
+xi = superdrops_variable_ensemble(ds, "xi")
+radius = superdrops_variable_ensemble(ds, "radius")
+msol = superdrops_variable_ensemble(ds, "msol")
+m_water = {
+    e: mass_water(radius[e], msol[e], consts["RHO_L"], consts["RHO_SOL"])
+    for e in ds.ensemble.values
+}
+ds = incloud_numconc(ds, xi, radius, assign_to_dataset=True)[0]
+ds = mass_weighted_mean_volume_diameter(
+    ds, xi, m_water, consts["RHO_L"], assign_to_dataset=True
+)[0]
+ds = mass_weighted_volume_diameter_stddev(
+    ds, xi, m_water, consts["RHO_L"], assign_to_dataset=True
+)[0]
+ds
 
 
 # %%
@@ -256,7 +404,9 @@ def plot_hill_figure2(ds):
     nc0 = ds.numconc.sel(time=0, method="nearest").mean().values
     fig.suptitle("Fig. 2, N$_a$ = {:.0f} ".format(nc0) + "cm$^{-3}$")
 
-    lwc_10mins = ds.lwc.sel(time=600, method="nearest")
+    t10mins = 600  # time to plot [seconds]
+
+    lwc_10mins = ds.lwc.sel(time=t10mins, method="nearest")
     mean, err1, err2 = mean_stddev(lwc_10mins, dim="ensemble")
     plot_vertical_error_shading(
         axs[0],
@@ -271,17 +421,75 @@ def plot_hill_figure2(ds):
             "label": "time={:.0f}mins".format(lwc_10mins.time.values / 60),
         },
     )
+    axs[0].set_xlim(left=0)
     axs[0].legend()
     axs[0].set_xlabel("LWC / g m$^{-3}$")
 
-    axs[0].set_ylim([0, 3000])
+    numconc_d_10mins = ds.numconc_d.sel(time=t10mins, method="nearest")
+    mean, err1, err2 = mean_stddev(numconc_d_10mins, dim="ensemble")
+    plot_vertical_error_shading(
+        axs[1],
+        ds.height,
+        mean,
+        err1,
+        err2,
+        plot_mean=True,
+        shading_kwargs={"color": "blue", "alpha": 0.3},
+        mean_kwargs={
+            "color": "blue",
+        },
+    )
+    axs[1].set_xlim(left=0)
+    axs[1].set_xlabel("N$_d$ / cm$^{-3}$")
+
+    dvol_10mins = ds.dvol.sel(time=t10mins, method="nearest")
+    mean, err1, err2 = mean_stddev(dvol_10mins, dim="ensemble")
+    plot_vertical_error_shading(
+        axs[2],
+        ds.height,
+        mean,
+        err1,
+        err2,
+        plot_mean=True,
+        shading_kwargs={"color": "blue", "alpha": 0.3},
+        mean_kwargs={
+            "color": "blue",
+        },
+    )
+    axs[2].set_xscale("log")
+    axs[2].set_xlim([10, 100])
+    axs[2].set_xlabel("D$_{vol}$ / \u03BCm")
+
+    sigma_10mins = ds.dvol_sigma.sel(time=t10mins, method="nearest")
+    mean, err1, err2 = mean_stddev(sigma_10mins, dim="ensemble")
+    plot_vertical_error_shading(
+        axs[3],
+        ds.height,
+        mean,
+        err1,
+        err2,
+        plot_mean=True,
+        shading_kwargs={"color": "blue", "alpha": 0.3},
+        mean_kwargs={
+            "color": "blue",
+        },
+    )
+    axs[3].set_xscale("log")
+    axs[3].set_xlim([0.1, 100])
+    axs[3].set_xlabel("\u03C3 / \u03BCm")
+
     for ax in axs:
         ax.set_ylabel("height / m")
+    ax.set_ylim([0, 3000])  # is for all axes when sharey==True
 
     fig.tight_layout()
 
+    return fig, axs
 
-plot_hill_figure2(ds)
+
+fig, axs = plot_hill_figure2(ds)
+savename = args.figpath / "hill_figure2.png"
+save_figure(fig, savename)
 plt.show()
 
 
