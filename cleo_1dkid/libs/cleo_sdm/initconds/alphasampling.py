48b3f21a114f86aa6e40456703677f8320352408
clara.bayley <clara.bayley@mpimet.mpg.de>
2025-12-09 10:42:24 +0100

    refactor: use updated alpha sampling API

--- cleo_1dkid/libs/cleo_sdm/initconds/alphasampling.py
+++ cleo_1dkid/libs/cleo_sdm/initconds/alphasampling.py
@@ -15,17 +15,30 @@ File Description:
 Wrapper around PySDM aloha sampling functions (see create_initsuperdropsbinary_script.py)
 """
 
+import numpy as np
 from PySDM.initialisation.sampling.spectral_sampling import AlphaSampling
+from PySDM.backends import CPU
 
 
 def AlphaSamplingWrapper(probdistrib, alpha, size_range):
-    alpha_sampling = AlphaSampling(probdistrib, alpha=alpha, size_range=size_range)
+    alpha_sampling = AlphaSampling(
+        probdistrib,
+        alpha=alpha,
+        size_range=size_range,
+        interp_points=100000,
+        dist_1_inv=lambda y, size_range: np.exp(
+            (np.log(size_range[1]) - np.log(size_range[0])) * y + np.log(size_range[0])
+        ),
+    )
 
     def alpha_sampling_xi(radii, totxi):
         return alpha_sampling_xi.xi
 
     def alpha_sampling_radii(nsupers):
-        radii, alpha_sampling_xi.xi = alpha_sampling.sample(nsupers)
+        backend = CPU()
+        radii, alpha_sampling_xi.xi = alpha_sampling.sample_quasirandom(
+            nsupers, backend=backend
+        )
         return radii
 
     return alpha_sampling_radii, alpha_sampling_xi
