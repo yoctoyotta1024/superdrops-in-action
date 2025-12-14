f774c736dd67052ef4ac24d0f78cefea9dfd1f4b
clara.bayley <clara.bayley@mpimet.mpg.de>
2025-12-14 18:19:45 +0100

    fix: need max_step in solve_ivp to get accurate dry density profile like in PySDM example
    see: https://github.com/open-atmos/PySDM/blob/e22f01ea1f4a5272965b0867e7104f1fc5dd101b/examples/PySDM_examples/Shipway_and_Hill_2012/settings.py#L142

--- cleo_1dkid/libs/test_case_1dkid/settings.py
+++ cleo_1dkid/libs/test_case_1dkid/settings.py
@@ -129,6 +129,7 @@ class Settings:
                 t_span=(0, self.z_max),
                 y0=np.asarray((rhod0,)),
                 t_eval=z_points,
+                max_step=self.dz / 2,
             )
             assert rhod_solution.success
 
